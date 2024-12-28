8046caed07e21d8b9c90f40f3447e01223b28f52
BenedictDunlap <Benedict_Dunlap@proton.me>
2024-12-28 21:20:41 +0100

    main.ts avec les instructions de https://docs.bsky.app/docs/starter-templates/bots

--- main.ts
+++ main.ts
@@ -1,7 +1,40 @@
+import { BskyAgent } from '@atproto/api';
+import * as dotenv from 'dotenv';
+import { CronJob } from 'cron';
+import * as process from 'process';
+
+dotenv.config();
+
 const port = parseInt(Deno.env.get("PORT") ?? "8000");
 
 function hello_world(_req: Request): Response {
     return new Response("Hello, world!");
 }
 
-Deno.serve({port: port}, hello_world);
+// Create a Bluesky Agent 
+const agent = new BskyAgent({
+    service: 'https://bsky.social',
+  })
+
+
+async function main() {
+    await agent.login({ identifier: process.env.BLUESKY_USERNAME!, password: process.env.BLUESKY_PASSWORD!})
+    await agent.post({
+        text: ":)"
+    });
+    console.log("Just posted!")
+}
+
+main();
+
+
+// Run this on a cron job
+const scheduleExpressionMinute = '* * * * *'; // Run once every minute for testing
+const scheduleExpression = '0 */3 * * *'; // Run once every three hours in prod
+
+const job = new CronJob(scheduleExpression, main); // change to scheduleExpressionMinute for testing
+
+job.start();
+
+
+Deno.serve({port: port}, job.start());
