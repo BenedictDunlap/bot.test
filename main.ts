a853a91fa00afe870679c9d3c05074cfa1efca0d
BenedictDunlap <Benedict_Dunlap@proton.me>
2024-12-28 21:40:48 +0100

    main.ts avec les instructions de https://docs.bsky.app/docs/starter-templates/bots et quelques modif d'erreur

--- main.ts
+++ main.ts
@@ -1,6 +1,5 @@
 import { BskyAgent } from '@atproto/api';
 import * as dotenv from 'dotenv';
-import { CronJob } from 'cron';
 import * as process from 'node:process';
 
 dotenv.config();
@@ -28,13 +27,4 @@ async function main() {
 main();
 
 
-// Run this on a cron job
-const scheduleExpressionMinute = '* * * * *'; // Run once every minute for testing
-const scheduleExpression = '0 */3 * * *'; // Run once every three hours in prod
-
-const job = new CronJob(scheduleExpression, main); // change to scheduleExpressionMinute for testing
-
-// job.start();
-
-
-Deno.serve({port: port}, job.start());
+Deno.serve({port: port}, main());
